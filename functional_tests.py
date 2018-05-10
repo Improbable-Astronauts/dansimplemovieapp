a25df7602282d58273230a19cf9955a9d12b960d
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-05-09 20:33:51 -0700

    Functional test now checks we can input a movie title

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,6 +12,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Margo likes movies and keeps a list of the movies they have watched.
         # They heard about a new online app that allows the user to keep a list of
@@ -18,18 +21,35 @@ class NewVisitorTest(unittest.TestCase):
         
         # They notice that they page title and header mention movie lists
         self.assertIn('Movie Lists', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Movie List', header_text)
         
         # They are invited to enter a movie title straight away
+        inputbox = self.browser.find_element_by_id('id_new_movie')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a movie title'
+        )
         
         # They type "The Terminator" into a text box (Margo remembers when movies
         # were original)
+        inputbox.send_keys('The Terminator')
         
         # When they hit enter, the page updates, and now the page lists
         # "1: The Terminator" as a movie title in a movie list
-        
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_movie_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: The Terminator' for row in rows)
+        )
+
+
         # There is still a text box inviting them to add another item. They
         # enter "Finding Nemo" (Margo has a nephew visit from time to time)
+        self.fail('Finish the test!')
         
         # The page updates again, and now shows both movie titles on their list
         
