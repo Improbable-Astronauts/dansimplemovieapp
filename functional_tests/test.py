69b02620de8a5f03c8bf6564efe22e160f0e17e8
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-05-15 14:08:10 -0700

    Add FT for multiple lists

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,7 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
+
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -13,13 +17,21 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_movie_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_movie_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Margo likes movies and keeps a list of the movies they have watched.
         # They heard about a new online app that allows the user to keep a list of
         # movies. Margo visits the movielists homepage
@@ -44,26 +56,60 @@ class NewVisitorTest(LiveServerTestCase):
         # When they hit enter, the page updates, and now the page lists
         # "1: The Terminator" as a movie title in a movie list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: The Terminator')
+        self.wait_for_row_in_list_table('1: The Terminator')
 
         # There is still a text box inviting them to add another item. They
         # enter "Finding Nemo" (Margo has a nephew visit from time to time)
         inputbox = self.browser.find_element_by_id('id_new_movie')
         inputbox.send_keys('Finding Nemo')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
         # The page updates again, and now shows both movie titles on their list
-        self.check_for_row_in_list_table('1: The Terminator')
-        self.check_for_row_in_list_table('2: Finding Nemo')
-        
-        # Margo wonders whether the site will remember their list. Then they see that
-        # the site has generated a unique URL for them -- there is some explanatory
-        # text to that effect.
-        self.fail('Finish the test!')
-        
-        # They visit that URL - their movie list is still there.
+        self.wait_for_row_in_list_table('2: Finding Nemo')
+        self.wait_for_row_in_list_table('1: The Terminator')
         
         # Satisfied, Margo goes back to sleep.
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Margo starts a new movie list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_movie')
+        inputbox.send_keys('The Terminator')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. The Terminator')
+
+        # They notice that their list has a unique URL
+        margo_list_url = self.browser.current_url
+        self.assertRegex(margo_list_url, '/lists/.+')
+
+        # Now a new user, Carlos, comes along to the site.
+        
+        ## We use a new browser session to make sure that no information
+        ## of Margo's is coming through from cookies, etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Carlos visits the home page. There is no sign of Margo's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('The Terminator', page_text)
+        self.assertNotIn('Finding Nemo', page_text)
+
+        # Carlos starts a new list by entering a new movie title.
+        # They are less interesting than Margo
+        inputbox = self.browser.find_element_by_id('id_new_movie')
+        inputbox.send_keys("You've Got Mail")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("You've Got Mail")
+
+        # Carlos gets their own unique URL
+        carlos_list_url = self.browser.current_url
+        self.assertRegex(carlos_list_url, '/lists/.+')
+        self.assertNotEqual(carlos_list_url, margo_list_url)
+
+        # Again, there is no trace of Margo's List
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('The Terminator', page_text)
+        self.assertIn("You've Got Mail", page_text)
+        
+        # Satisfied, they both go back to sleep.
